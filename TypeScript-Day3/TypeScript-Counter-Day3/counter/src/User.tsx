7a4deca5efbac7b6ac6cd7e387110fedd7f84f21
Parami <paramiashinsana@gmail.com>
2024-05-26 13:36:29 +0530

    TypeScript-Counter-Day3

---
+++ TypeScript-Day3/TypeScript-Counter-Day3/counter/src/User.tsx
@@ -0,0 +1,21 @@
+function User() {
+    const name = "Parami";
+    const age = 30;
+    const bestFriend = "Sh";
+  
+    return (
+        // Fragments
+      <>
+        <h1>User Information</h1>
+        <h2>My name is {name}</h2>
+        <h2>I'm {age} years old</h2>
+        <h2>My best friend is {bestFriend}</h2>
+      </>
+    );
+  }
+  
+  export default User;
+  
+
+
+  
