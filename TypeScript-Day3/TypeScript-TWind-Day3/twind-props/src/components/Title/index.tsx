6b54d772025f9dbf5ec95c8bca584d6c5918d42d
Parami <paramiashinsana@gmail.com>
2024-05-26 13:46:19 +0530

    TypeScript-TailWind-Day3

---
+++ TypeScript-Day3/TypeScript-TWind-Day3/twind-props/src/components/Title/index.tsx
@@ -0,0 +1,9 @@
+export default function Titles(props) {
+    return (
+        <>  
+        <p className="text-base text-gray-700 leading-relaxed mb-4">
+            {props.name}
+        </p>
+        </>
+    );
+  }
