ccd627f0234dcb34430bafa648b937c5c04e0cc9
Parami <paramiashinsana@gmail.com>
2024-05-26 13:37:21 +0530

    TypeScript-Counter-Day3

---
+++ TypeScript-Day3/TypeScript-Counter-Day3/counter/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+// import User from "./User";
+// import Item from "./Item";
+import Counter from './Counter';
+import Task from './Task';
+
+function App() {
+  return (
+    <>
+    {/* <User />
+      <Item /> */}
+      {/* <Counter /> */}
+      <Task />
+    </>
+      
+  );
+}
+
+export default App;
